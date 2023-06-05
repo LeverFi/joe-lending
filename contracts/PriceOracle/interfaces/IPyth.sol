21405f8290f83288eb78e57ffc5435884a78d6e4
ANU B NAIR <anubnair90@gmail.com>
2023-06-05 21:38:52 +0800

    Create IPyth.sol

---
+++ contracts/PriceOracle/interfaces/IPyth.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+library PythStructs {
+    // A price with a degree of uncertainty, represented as a price +- a confidence interval.
+    //
+    // The confidence interval roughly corresponds to the standard error of a normal distribution.
+    // Both the price and confidence are stored in a fixed-point numeric representation,
+    // `x * (10^expo)`, where `expo` is the exponent.
+    //
+    // Please refer to the documentation at https://docs.pyth.network/consumers/best-practices for how
+    // to how this price safely.
+    struct Price {
+        // Price
+        int64 price;
+        // Confidence interval around the price
+        uint64 conf;
+        // Price exponent
+        int32 expo;
+        // Unix timestamp describing when the price was published
+        uint publishTime;
+    }
+
+    // PriceFeed represents a current aggregate price from pyth publisher feeds.
+    struct PriceFeed {
+        // The price ID.
+        bytes32 id;
+        // Latest available price
+        Price price;
+        // Latest available exponentially-weighted moving average price
+        Price emaPrice;
+    }
+}
+
+/// @title IPythEvents contains the events that Pyth contract emits.
+/// @dev This interface can be used for listening to the updates for off-chain and testing purposes.
+interface IPythEvents {
+    /// @dev Emitted when the price feed with `id` has received a fresh update.
+    /// @param id The Pyth Price Feed ID.
+    /// @param publishTime Publish time of the given price update.
+    /// @param price Price of the given price update.
+    /// @param conf Confidence interval of the given price update.
+    event PriceFeedUpdate(
+        bytes32 indexed id,
+        uint64 publishTime,
+        int64 price,
+        uint64 conf
+    );
+
+    /// @dev Emitted when a batch price update is processed successfully.
+    /// @param chainId ID of the source chain that the batch price update comes from.
+    /// @param sequenceNumber Sequence number of the batch price update.
+    event BatchPriceFeedUpdate(uint16 chainId, uint64 sequenceNumber);
+}
+
+/// @title Consume prices from the Pyth Network (https://pyth.network/).
+/// @dev Please refer to the guidance at https://docs.pyth.network/consumers/best-practices for how to consume prices safely.
+/// @author Pyth Data Association
+interface IPyth is IPythEvents {
+    /// @notice Returns the price and confidence interval.
+    /// @dev Reverts if the price has not been updated within the last `getValidTimePeriod()` seconds.
+    /// @param id The Pyth Price Feed ID of which to fetch the price and confidence interval.
+    /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
+    function getPrice(
+        bytes32 id
+    ) external view returns (PythStructs.Price memory price);
+}
