b8e8af57c675115bcee157e351e6350e59aea002
Anu <anubnair90@gmail.com>
2023-06-07 15:45:25 +0800

    updated pyth adapter

--- contracts/PriceOracle/PythOracleAdapter.sol
+++ contracts/PriceOracle/PythOracleAdapter.sol
@@ -2,43 +2,108 @@
 
 pragma solidity ^0.8.0;
 
-import {IPyth, PythStructs} from "./interfaces/IPyth.sol";
-
-contract PythOracleAdapter {
-
-    IPyth public immutable pyth;
-
-    bytes32 public immutable assetID;
-
-    uint8 public immutable decimals;
-
-    constructor(IPyth pyth_, bytes32 assetID_) {
-        pyth = pyth_;
-        assetID = assetID_;
-
-        PythStructs.Price memory price = pyth.getPrice(assetID);
-
-        uint32 absExpo = uint32(price.expo * (- 1));
-
-        require(absExpo <= type(uint8).max, "overflow exponent");
-
-        // should be safe
-        decimals = uint8(absExpo);
+library PythStructs {
+    // A price with a degree of uncertainty, represented as a price +- a confidence interval.
+    //
+    // The confidence interval roughly corresponds to the standard error of a normal distribution.
+    // Both the price and confidence are stored in a fixed-point numeric representation,
+    // `x * (10^expo)`, where `expo` is the exponent.
+    //
+    // Please refer to the documentation at https://docs.pyth.network/consumers/best-practices for how
+    // to how this price safely.
+    struct Price {
+        // Price
+        int64 price;
+        // Confidence interval around the price
+        uint64 conf;
+        // Price exponent
+        int32 expo;
+        // Unix timestamp describing when the price was published
+        uint256 publishTime;
     }
 
-    function latestRoundData() public view returns (
-        uint80 roundId,
-        int256 answer,
-        uint256 startedAt,
-        uint256 updatedAt,
-        uint80 answeredInRound
-    ) {
+    // PriceFeed represents a current aggregate price from pyth publisher feeds.
+    struct PriceFeed {
+        // The price ID.
+        bytes32 id;
+        // Latest available price
+        Price price;
+        // Latest available exponentially-weighted moving average price
+        Price emaPrice;
+    }
+}
+
+/// @title IPythEvents contains the events that Pyth contract emits.
+/// @dev This interface can be used for listening to the updates for off-chain and testing purposes.
+interface IPythEvents {
+    /// @dev Emitted when the price feed with `id` has received a fresh update.
+    /// @param id The Pyth Price Feed ID.
+    /// @param publishTime Publish time of the given price update.
+    /// @param price Price of the given price update.
+    /// @param conf Confidence interval of the given price update.
+    event PriceFeedUpdate(bytes32 indexed id, uint64 publishTime, int64 price, uint64 conf);
+
+    /// @dev Emitted when a batch price update is processed successfully.
+    /// @param chainId ID of the source chain that the batch price update comes from.
+    /// @param sequenceNumber Sequence number of the batch price update.
+    event BatchPriceFeedUpdate(uint16 chainId, uint64 sequenceNumber);
+}
+
+/// @title Consume prices from the Pyth Network (https://pyth.network/).
+/// @dev Please refer to the guidance at https://docs.pyth.network/consumers/best-practices for how to consume prices safely.
+/// @author Pyth Data Association
+interface IPyth is IPythEvents {
+    /// @notice Returns the price and confidence interval.
+    /// @dev Reverts if the price has not been updated within the last `getValidTimePeriod()` seconds.
+    /// @param id The Pyth Price Feed ID of which to fetch the price and confidence interval.
+    /// @return price - please read the documentation of PythStructs.Price to understand how to use this safely.
+    function getPrice(bytes32 id) external view returns (PythStructs.Price memory price);
+}
+
+contract PythOracleAdapter {
+    IPyth pyth;
+
+    mapping(address => bytes32) public assetIDs;
+
+    mapping(address => uint8) public decimals;
+
+    constructor(address pyth_) {
+        pyth = IPyth(pyth_);
+    }
+
+    function setAssetId(address[] memory assets, bytes32[] memory assetID) external {
+        PythStructs.Price memory price = PythStructs.Price(0, 0, 0, 0);
+        uint32 absExpo = 0;
+
+        for (uint256 i = 0; i < assets.length; i++) {
+            assetIDs[assets[i]] = assetID[i];
+
+            price = pyth.getPrice(assetID[i]);
+
+            if (price.expo < 0) {
+                absExpo = uint32(price.expo * (-1));
+            } else {
+                absExpo = uint32(price.expo);
+            }
+
+            require(absExpo <= type(uint8).max, "overflow exponent");
+
+            // should be safe
+            decimals[assets[i]] = uint8(absExpo);
+        }
+    }
+
+    function latestRoundData(address asset)
+    external
+    view
+    returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
         // Shh, not in use
         roundId = 0;
         startedAt = 0;
         answeredInRound = 0;
 
-        PythStructs.Price memory price = pyth.getPrice(assetID);
+        PythStructs.Price memory price = pyth.getPrice(assetIDs[asset]);
 
         answer = int256(price.price);
         updatedAt = uint256(price.publishTime);
